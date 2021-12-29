88062788bd0a25447ebc7b666f625caeda8748f1
Kirill Poltoranin <kirillpoltoranin@gmail.com>
2021-12-29 14:17:57 +0700

    АПИ для получения инфы из файлового хранилища по HTTP запросам

---
+++ DirectoriesBackend/DirectoriesBackend/Controllers/DirectoryController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectoriesBackend.Controllers
+{
+    [ApiController]
+    [Route("/directory")]
+    public class DirectoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("/info")]
+        public IActionResult GetDirectoryInfo(string directory)
+        {
+            try
+            {
+                var dirInfo = new DirectoryInfo(directory);
+                var directoryInfo = "";
+                foreach (var dir in dirInfo.GetDirectories()) 
+                {
+                    directoryInfo += dir.Name + '\n';
+                }
+                foreach (var file in dirInfo.GetFiles()) 
+                {
+                    directoryInfo += file.Name + '\n';
+                }
+                return Ok(directoryInfo);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("/file")]
+        public IActionResult GetFileInfo(string filePath)
+        {
+            try
+            {
+                return Ok(System.IO.File.ReadAllText(filePath));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+            
+        }
+
+    }
+}
